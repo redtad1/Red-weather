83bd127e0f3a357148b29a1b8365cf3db13662a9
redtad1 <rediat1b@gmail.com>
2023-06-19 00:14:35 -0500

    update time and date

--- src/app.js
+++ src/app.js
@@ -1,4 +1,43 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = "Monday 11:10";
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
 let apikey = "3b2bf4c07afb429c58d48fd405810a82";
 let apiurl = `https://api.openweathermap.org/data/2.5/weather?q=chicago&appid=${apikey}&units=metric`;
 
-console.log(apiurl);
+axios.get(apiurl).then(displayTemperature);
